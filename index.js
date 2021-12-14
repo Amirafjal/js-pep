018e799c6090a41835578079674290668464146a
Amirafjal <85572222+Amirafjal@users.noreply.github.com>
2021-12-14 23:03:57 +0530

    js

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
+// print
 console.log("Hello JS");
+// declare variable
+let a;
+// console.log("variable :", a);
+a = 10;
+// console.log("variable contains :", a);
+a = 10.1;
+// console.log("variable contains :", a);
+a = "Hello I am a String";
+// console.log("variable contains :", a);
+a = 'Hello I am also a string';
+// console.log("variable contains :", a);
+
+// print number 
+
+// let number = 10;
+// for (let i = 1; i <= number; i++) {
+//     console.log("Number is :", i)
+// }
+
+// even number
+
+// let number = 20;
+// for (let i = 0; i <= number; i++) {
+//     if (i % 2 == 0) {
+//         console.log("even numbers are :", i)
+
+//     }
+// }
+
+// ODD number
+let number = 20;
+for (let i = 0; i <= number; i++) {
+    if (i % 2 != 0) {
+        console.log("these are odd numbers :", i)
+    }
+}
