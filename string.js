4a8085305220702a3c5859c65bb0c3bfca83ee28
Amirafjal <85572222+Amirafjal@users.noreply.github.com>
2021-12-16 03:42:08 +0530

    string

---
+++ string.js
@@ -0,0 +1,39 @@
+// string.js
+
+// let singleQuotes = 'single quotes ki string';
+// let doubleQuotes = "double quotes ki string";
+
+// console.log(singleQuotes);
+// console.log(doubleQuotes);
+
+// let char = singleQuotes.charAt(4);
+// let substr = singleQuotes.substring(1, 4);
+// let ascii = singleQuotes.charCodeAt(4);
+// console.log(char);
+// console.log(substr);
+// console.log(ascii);
+
+// split and join->
+
+// let singleQuotes = 'single quotes ki string';
+// let doubleQuotes = 'double quotes ki string';
+
+// let arrStr = singleQuotes.split(" ");
+// console.log(arrStr);
+
+// let str = arrStr.join("+");
+// console.log(str);
+// console.log("-----------")
+
+// -----------------------------------
+// trim and join concept 
+// trim concept not clear go through it once   
+
+// let singleQuotes = 'single quotes ki string';
+// let arrStr = singleQuotes.split(" ");
+
+// let singlequote = singleQuotes.trim();
+// let str = arrStr.join("$");
+
+// console.log(singlequote);
+// console.log(str);
